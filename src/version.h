622cdd700b7a0265bfef43987cc56cd1690e1abc
John E. Davis <jed@jedsoft.org>
2021-12-04 15:09:24 -0500

    v0.6.3: Removed top-level Makefile

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define MODULE_VERSION_STRING   "0.6.2"
-#define MODULE_VERSION_NUMBER   602
+#define MODULE_VERSION_STRING   "0.6.3"
+#define MODULE_VERSION_NUMBER   603
 
 static char *Module_Version_String = MODULE_VERSION_STRING;
